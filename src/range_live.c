031d447b5c2204415b81dab92daa225fbb4d5d95
kerrymilan <me@kerrymilan.com>
2023-04-13 06:53:14 -0400

    Live range display cleanup
     * Shorten label text
     * Call controller_scan() before get_keys_pressed()
     * Center elements horizontally
     * Whitespace cleanup

--- src/range_live.c
+++ src/range_live.c
@@ -43,22 +43,20 @@ struct StickAngles *live_comparisons[] = {
     &live_compare_hori,
 };
 
-char * get_comparison_str(int current_comparison) {
+char *get_title_str(int current_comparison) {
     switch (current_comparison) {
-        case 0:
-            return "";
         case 1:
-            return ", ideal OEM comparison";
+            return "Live, ideal OEM overlay";
         case 2:
-            return ", ideal Hori Minipad comparison";
+            return "Live, ideal Horipad Mini overlay";
     }
-    return "";
+    return "Live range display";
 }
 
 void display_live_ranges() {
-    int count = 0, 
+    int count = 0,
         line_height = 11,
-        show_history = 1, 
+        show_history = 1,
         sz_history = 1024,
         current_comparison = 1,
         comparison_count = sizeof(live_comparisons) / sizeof(0);
@@ -67,7 +65,7 @@ void display_live_ranges() {
 
     int f = dfs_open("/gfx/point.sprite");
     int size = dfs_size(f);
-    char * comparison_str = "";
+    char * title_str = get_title_str(current_comparison);
     sprite_t *point = malloc(size);
     dfs_read(point, size, 1, f);
     dfs_close(f);
@@ -84,33 +82,30 @@ void display_live_ranges() {
         graphics_fill_screen(ctx, COLOR_BACKGROUND);
         graphics_set_color(COLOR_FOREGROUND, 0);
 
+        controller_scan();
         struct controller_data cdata = get_keys_pressed();
         char buf[128];
 
-        controller_scan();
         struct Vec2 v = { cdata.c[0].x, cdata.c[0].y };
 
         snprintf(buf, sizeof(buf), "x\ny");
         text_set_font(FONT_MEDIUM);
-        text_draw(ctx, 270, 120 - line_height, buf, ALIGN_LEFT);
+        text_draw(ctx, 290, 120 - line_height, buf, ALIGN_LEFT);
 
         text_set_font(FONT_BOLD);
         snprintf(buf, sizeof(buf), "%3d\n%3d", v.x, v.y);
-        text_draw(ctx, 263, 120 - line_height, buf, ALIGN_RIGHT);
+        text_draw(ctx, 282, 120 - line_height, buf, ALIGN_RIGHT);
 
         draw_center_cross(ctx, 160);
         if (current_comparison > 0) {
             draw_stick_angles(
-                ctx, 
-                *live_comparisons[current_comparison], 
+                ctx,
+                *live_comparisons[current_comparison],
                 comparison_color,
                 0,
                 160
             );
         }
-
-        snprintf(buf, sizeof(buf), "Live range display%s", comparison_str);
-        text_draw(ctx, 160, 15, buf, ALIGN_CENTER);
 
         if (show_history == 1) {
             int history_update = 0;
@@ -125,13 +120,13 @@ void display_live_ranges() {
 
             for (int i = count; i > 0; i--) {
                 if (history_update == 1) history[i] = history[i - 1];
-                int x = smax(0, smin(320, history[i].x + 120));
+                int x = smax(0, smin(320, history[i].x + 160));
                 int y = smax(0, smin(240, (history[i].y * -1) + 120));
-                graphics_draw_pixel( ctx, x, y, history_color);
-            } 
+                graphics_draw_pixel(ctx, x, y, history_color);
+            }
         }
 
-        int x = smax(0, smin(320, v.x + 118));
+        int x = smax(0, smin(320, v.x + 158));
         int y = smax(0, smin(240, (v.y * -1) + 118));
         graphics_draw_sprite(ctx, x, y, point);
 
@@ -155,13 +150,17 @@ void display_live_ranges() {
         if (cdata.c[0].left || cdata.c[0].L) {
             current_comparison--;
             if (current_comparison < 0) current_comparison += comparison_count;
-            comparison_str = get_comparison_str(current_comparison);
+            title_str = get_title_str(current_comparison);
         }
 
         if (cdata.c[0].right || cdata.c[0].R) {
             current_comparison = (current_comparison + 1) % comparison_count;
-            comparison_str = get_comparison_str(current_comparison);
+            title_str = get_title_str(current_comparison);
         }
+
+        text_set_font(FONT_MEDIUM);
+        snprintf(buf, sizeof(buf), "%s", title_str);
+        text_draw(ctx, 160, 15, buf, ALIGN_CENTER);
     }
 
     free(point);
